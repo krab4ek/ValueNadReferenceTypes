4b25c197c0816b9f3c307ec438faf230e1db3f20
Covali Andrei <krab4ek@gmail.com>
2022-06-25 19:22:02 +0300

    final

--- ValueNadReferenceTypes/Program.cs
+++ ValueNadReferenceTypes/Program.cs
@@ -1,5 +1,56 @@
 ﻿ValueTypeAssigning();
 ReferenceTypeAssigning();
+ValueTypeContainingRefType();
+#region 
+Console.WriteLine("***** Passing Person object by value *****");
+Person fred = new Person("Fred", 22);
+
+Console.WriteLine("\nBefore by value call, Person is:");
+fred.Display();
+//Console.WriteLine("\nSendAPersonByValue()");
+//SendAPersonByValue(fred);
+Console.WriteLine("\nSendAPersonByReference()");
+SendAPersonByReference(ref fred);
+Console.WriteLine("\nAfter by value call, Person is:");
+fred.Display();
+Console.ReadLine();
+
+
+#endregion
+
+static void SendAPersonByReference(ref Person p)
+{
+    //Изменить значение возраста в p
+    p.personAge = 555;
+    //Увидит ли вызывающий код это выражение?
+    p = new Person("Nikki", 99);
+}
+
+static void SendAPersonByValue(Person p)
+{
+    //Изменить значение возраста в p
+    p.personAge = 99;
+    //Увидит ли вызывающий код это выражение?
+    p = new Person("Andrei", 99);
+}
+
+static void ValueTypeContainingRefType()
+{
+    //Создать первую переменную Rectangle
+    Console.WriteLine("=> Creating r1");
+    Rectangle r1 = new Rectangle("First Rect", 10, 10, 60, 56);
+    r1.Display();
+    //присвоить новой перменной Rectangle перменную r1
+    Console.WriteLine("=> Assigning r2 to r1");
+    Rectangle r2 = r1;
+    //Изменить некоторые значения в r2
+    Console.WriteLine("=. Changing values of r2");
+    r2.RectInfo.InfoString = "This is new info!";
+    r2.RectLeft = 55555;
+    r1.Display();
+    r2.Display();
+
+}
 
 static void ValueTypeAssigning()
 {
@@ -30,6 +81,56 @@ static void ReferenceTypeAssigning()
     p2.Display();
     Console.WriteLine("__________________________________________");
 }
+class PointRef
+{
+    //Поля структуры
+    public int X;
+    public int Y;
+
+    public PointRef(int XPos, int YPos)
+    {
+        X = XPos;
+        Y = YPos;
+    }
+
+    public void Increment()
+    {
+        X++; Y++;
+    }
+
+    public void Decrement()
+    {
+        X--; Y--;
+    }
+
+    public void Display() => Console.WriteLine($"X={X}, Y={Y}");
+
+}
+class ShapeInfo
+{
+    public string InfoString;
+    public ShapeInfo(string info)
+    {
+        InfoString = info;
+    }
+}
+
+class Person
+{
+    public string personName;
+    public int personAge;
+    //Конструкторы
+    public Person(
+        string name,
+        int age)
+    {
+        personAge = age;
+        personName = name;
+    }
+    public void Display() => Console.WriteLine($"Name:{personName}, Age:{personAge}");
+
+}
+
 struct Point
 {
     //Поля структуры
@@ -57,29 +158,22 @@ struct Point
         Console.WriteLine($"X={X}, Y={Y}");
     }
 }
-
-class PointRef
+struct Rectangle
 {
-    //Поля структуры
-    public int X;
-    public int Y;
-
-    public PointRef(int XPos, int YPos)
+    //структура Rectangle содержит член ссылочного типа
+    public ShapeInfo RectInfo;
+    public int RectTop, RectLeft, RectBottom, RectRight;
+    public Rectangle(string info, int top, int left, int bottom, int right)
     {
-        X = XPos;
-        Y = YPos;
+        RectInfo = new ShapeInfo(info);
+        RectTop = top; RectLeft = left;
+        RectBottom = bottom; RectRight = right;
     }
-
-    public void Increment()
+    public void Display()
     {
-        X++; Y++;
+        Console.WriteLine($"String = {RectInfo.InfoString}, Top = {RectTop}," +
+            $" Bottom={RectBottom}, Left = {RectLeft}, Right = {RectRight}");
     }
-
-    public void Decrement()
-    {
-        X--; Y--;
-    }
-
-    public void Display() => Console.WriteLine($"X={X}, Y={Y}");
-
 }
+
+
